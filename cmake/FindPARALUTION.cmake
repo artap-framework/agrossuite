3103c3895c462d35b9c378450c1b0ade1826684f
Lukas Korous <lukas.korous@gmail.com>
2015-03-06 13:41:43 +0100

    Make PARALUTION an in-tree build in 3rdparty.

--- cmake/FindPARALUTION.cmake
+++ cmake/FindPARALUTION.cmake
@@ -5,11 +5,15 @@
 
 FIND_PATH(PARALUTION_INCLUDE_DIR paralution.hpp ${PARALUTION_ROOT}/include ${PARALUTION_ROOT}/inc /usr/local/include /usr/include)
 
-if(WIN64)
-  FIND_LIBRARY(PARALUTION_LIBRARY NAMES paralution PATHS ${PARALUTION_ROOT} ${PARALUTION_ROOT}/lib/x64 ${PARALUTION_ROOT}/lib)
-else(WIN64)  
-  FIND_LIBRARY(PARALUTION_LIBRARY NAMES paralution PATHS ${PARALUTION_ROOT} ${PARALUTION_ROOT}/lib /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64)
-endif(WIN64)
+IF(WIN64)
+  FIND_LIBRARY(PARALUTION_LIBRARY NAMES paralution PATHS ${PARALUTION_ROOT} ${PARALUTION_ROOT}/lib/x64 ${PARALUTION_ROOT}/x64/lib ${PARALUTION_ROOT}/x64/${CMAKE_BUILD_TYPE})
+ELSE(WIN64)
+  IF(WIN32)
+    FIND_LIBRARY(PARALUTION_LIBRARY NAMES paralution PATHS ${PARALUTION_ROOT} ${PARALUTION_ROOT}/lib ${PARALUTION_ROOT}/lib/${CMAKE_BUILD_TYPE})
+  ELSE(WIN32)
+    FIND_LIBRARY(PARALUTION_LIBRARY NAMES paralution PATHS ${PARALUTION_ROOT} ${PARALUTION_ROOT}/lib /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64)
+  ENDIF(WIN32)
+ENDIF(WIN64)
 
 # Report the found libraries, quit with fatal error if any required library has not been found.
 INCLUDE(FindPackageHandleStandardArgs)
