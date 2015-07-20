ec4b87847f7b17b604cc3635681db32e680b8954
Pavel Karban <pkarban@gmail.com>
2015-07-20 21:19:58 +0200

    gmsh removed

--- 3rdparty/gmsh/Numeric/miniBasis.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MINI_BASIS_H_
-#define _MINI_BASIS_H_
-#include "polynomialBasis.h"
-class miniBasis : public polynomialBasis { // mini is NOT a real nodal basis but in GMSH, only the nodal basis have closure and mini have closure so...
-  public:
-    miniBasis();
-};
-#endif
