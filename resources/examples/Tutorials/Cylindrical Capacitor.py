05da5030f57ec77a55f3627dbde7432e558f4348
Pavel Karban <pkarban@gmail.com>
2015-11-17 20:40:04 +0100

    fixed syntax - doesn't work

--- resources/examples/Tutorials/Cylindrical Capacitor.py
+++ resources/examples/Tutorials/Cylindrical Capacitor.py
@@ -10,7 +10,7 @@ problem.mesh_type = "triangle"
 
 # fields
 # electrostatic
-electrostatic = a2d.field("electrostatic")
+electrostatic = problem.field("electrostatic")
 electrostatic.analysis_type = "steadystate"
 electrostatic.number_of_refinements = 1
 electrostatic.polynomial_order = 2
@@ -40,10 +40,9 @@ electrostatic.add_material("Dielectric n.1", {"electrostatic_permittivity" : 10,
 electrostatic.add_material("Dielectric n.2", {"electrostatic_permittivity" : 3, "electrostatic_charge_density" : 0})
 
 # geometry
-geometry = a2d.geometry
+geometry = problem.geometry()
 
 # edges
-geometry = a2d.geometry
 geometry.add_edge(0, 1.5*l, 0, l/2,  boundaries = {"electrostatic" : "Neumann BC"})
 geometry.add_edge(r1, l/2, r1, 0, boundaries = {"electrostatic" : "Source"})
 geometry.add_edge(r1, 0, r2, 0,  boundaries = {"electrostatic" : "Neumann BC"})
@@ -61,22 +60,24 @@ geometry.add_edge(r1, l/2, 0, l/2, boundaries = {"electrostatic" : "Source"})
 geometry.add_label(0.019, 0.021, materials = {"electrostatic" : "Dielectric n.1"})
 geometry.add_label(0.0379, 0.051, materials = {"electrostatic" : "Dielectric n.2"})
 geometry.add_label(0.0284191, 0.123601, materials = {"electrostatic" : "Air"})
-a2d.view.zoom_best_fit()
 
 # calculation of capacity
+computation = problem.computation()
+
 r = []
 C = []
 print("C = f(r) (F):")
 for i in range(15):
-	if i > 0:
-		geometry.select_edges([6, 7, 8])
-		geometry.move_selection(dr, 0, False)
+    if i > 0:
+        geometry.select_edges([6, 7, 8])
+        geometry.move_selection(dr, 0, False)
 
-	problem.solve()
-	result = electrostatic.volume_integrals([0, 1, 2])
-	r.append(r1 + (i*dr))
-	C.append(2*2*result["We"]/(U^2))
-	print(r[-1], C[-1])
+    computation.solve()
+    solution = computation.solution("electrostatic")
+    result = solution.volume_integrals([0, 1, 2])
+    r.append(r1 + (i*dr))
+    C.append(2*2*result["We"]/(U^2))
+    print(r[-1], " = ", C[-1])
 
 # chart
 pl.close()
