f4b7894bd6a2d0a2b47b848d2eca23008e45eca2
František Mach <fmach@kte.zcu.cz>
2015-12-05 12:42:01 +0100

    script tests running

--- resources/test/test_suite/script/__init__.py
+++ resources/test/test_suite/script/__init__.py
@@ -1,7 +1,6 @@
-__all__ = ["benchmark", "field", "geometry", "problem", "script"]
+__all__ = ["benchmark", "field", "geometry", "problem"]
 
 from test_suite.script import problem
 from test_suite.script import field
 from test_suite.script import geometry
 from test_suite.script import benchmark
-from test_suite.script import script
