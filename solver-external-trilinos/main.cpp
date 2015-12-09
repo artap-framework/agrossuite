ca5192810e68733dea13a956a3d5b593f8ee67d1
Petr Kropik <pkropik@kte.zcu.cz>
2015-12-09 17:32:06 +0100

    modification of include pathes experiments with epetra and amesos, preparing for adat import into epetra matrix

--- solver-external-trilinos/main.cpp
+++ solver-external-trilinos/main.cpp
@@ -46,15 +46,72 @@
 #include <Epetra_SerialSymDenseMatrix.h>
 //----
 // develop. - Serial version, not yet prepared for MPI (page 14 of Trilinos pdf manual)
-//#include <mpi/mpi.h> // ??
+#include "mpi/mpi.h"
 #include <Epetra_Map.h>
 #include <Epetra_CrsGraph.h>
 #include <EpetraExt_RowMatrixOut.h>
 #include <EpetraExt_VectorOut.h>
-
+#include "AztecOO.h"
 //----
 #include "../3rdparty/tclap/CmdLine.h"
 #include "../util/sparse_io.h"
+
+void denseVersion(int numOfRows, int numOfNonZero, VectorRW &system_rhs, VectorRW &solution, double *matrixA, int *iRn, int *jCn)
+{
+    // -------------------------- Dense serial version - TODO: separate to class -----------
+    // --- develop ---
+    // print elements in (of the matrix row by row)
+    //        std::cout << "Matrixes -----" << std::endl;
+    //        for (int row = 0; row < system_matrix_pattern.rows; ++row)
+    //        {
+    //            std::size_t col_start = system_matrix_pattern.rowstart[row];
+    //            std::size_t col_end = system_matrix_pattern.rowstart[row + 1];
+
+    //            for (int i = col_start; i < col_end; i++)
+    //            {
+    //                std::cout << row << " " << system_matrix_pattern.colnums[i] << " " <<  system_matrix.val[i] << std::endl;
+    //            }
+    //        }
+
+            Epetra_SerialDenseVector epeRhs(numOfRows);     // it have automatic deletion of memory allocation when destructed
+            epeRhs.SetLabel("RHS - com Epetra");
+
+            // fill Epetra vector by rhs values
+            for (int i = 0; i < numOfRows; i++) {
+    //            std::cout << system_rhs[i] << " ";    // show original rhs values - develop
+                epeRhs(i)= system_rhs[i];             // (i) enforce bounds checking
+            }
+    //        std::cout << std::endl << "Filled RHS check" << std::endl << epeRhs << std::endl;  // develop
+
+
+            Epetra_SerialDenseMatrix epeA(numOfRows, numOfRows);
+            // fill the Epetra matrix A by solved system A matrix values
+            for (int index = 0; index < numOfNonZero; index++) {
+                epeA(iRn[index], jCn[index]) = matrixA[index];
+            }
+    //        std::cout << std::endl << "Filled A matrix check" << std::endl << epeA << std::endl;  // develop
+
+    //        develop - simple test of matrix multiplication
+    //        Epetra_SerialDenseMatrix epeMultiplication(numOfRows, numOfRows);
+    //        epeMultiplication.Multiply('Y', 'N', 1.0, epeA, epeA, 1.0);
+    //        std::cout << "Multiplication" << std::endl << epeMultiplication << std::endl;
+
+
+            Epetra_SerialDenseVector epeSolution(numOfRows);     // Epetra solution vector
+    // ---------------
+
+    //        Epetra_LinearProblem Problem(&A, &X, &rhs);
+            Epetra_SerialDenseSolver solver;
+            solver.SetMatrix(epeA);
+            solver.SetVectors(epeSolution, epeRhs);
+
+    //        solver.ApplyRefinement();
+            int solvStatus = solver.Solve();
+            std::cout << "Solver status: " << solvStatus << std::endl;
+            std::cout << "Solved ?: " << solver.Solved() << std::endl;
+            std::cout << "rcond = " << solver.RCOND() << std::endl;
+    // -------------------------- END of Dense version ------------------------------
+}
 
 int main(int argc, char *argv[])
 {
@@ -138,122 +195,70 @@ int main(int argc, char *argv[])
                 ++index;
             }
         }
-
-// -------------------------- Dense serial version - TODO: separate to class -----------
-// --- develop ---
-// print elements in (of the matrix row by row)
-//        std::cout << "Matrixes -----" << std::endl;
-//        for (int row = 0; row < system_matrix_pattern.rows; ++row)
-//        {
-//            std::size_t col_start = system_matrix_pattern.rowstart[row];
-//            std::size_t col_end = system_matrix_pattern.rowstart[row + 1];
-
-//            for (int i = col_start; i < col_end; i++)
-//            {
-//                std::cout << row << " " << system_matrix_pattern.colnums[i] << " " <<  system_matrix.val[i] << std::endl;
-//            }
-//        }
-
-        Epetra_SerialDenseVector epeRhs(numOfRows);     // it have automatic deletion of memory allocation when destructed
-        epeRhs.SetLabel("RHS - com Epetra");
-
-        // fill Epetra vector by rhs values
-        for (int i = 0; i < numOfRows; i++) {
-//            std::cout << system_rhs[i] << " ";    // show original rhs values - develop
-            epeRhs(i)= system_rhs[i];             // (i) enforce bounds checking
-        }
-//        std::cout << std::endl << "Filled RHS check" << std::endl << epeRhs << std::endl;  // develop
-
-
-        Epetra_SerialDenseMatrix epeA(numOfRows, numOfRows);
-        // fill the Epetra matrix A by solved system A matrix values
-        for (int index = 0; index < numOfNonZero; index++) {
-            epeA(iRn[index], jCn[index]) = matrixA[index];
-        }
-//        std::cout << std::endl << "Filled A matrix check" << std::endl << epeA << std::endl;  // develop
-
-//        develop - simple test of matrix multiplication
-//        Epetra_SerialDenseMatrix epeMultiplication(numOfRows, numOfRows);
-//        epeMultiplication.Multiply('Y', 'N', 1.0, epeA, epeA, 1.0);
-//        std::cout << "Multiplication" << std::endl << epeMultiplication << std::endl;
-
-
-        Epetra_SerialDenseVector epeSolution(numOfRows);     // Epetra solution vector
-// ---------------
-
-//        Epetra_LinearProblem Problem(&A, &X, &rhs);
-        Epetra_SerialDenseSolver solver;
-        solver.SetMatrix(epeA);
-        solver.SetVectors(epeSolution, epeRhs);
-
-//        solver.ApplyRefinement();
-        int solvStatus = solver.Solve();
-        std::cout << "Solver status: " << solvStatus << std::endl;
-        std::cout << "Solved ?: " << solver.Solved() << std::endl;
-        std::cout << "rcond = " << solver.RCOND() << std::endl;
-// -------------------------- END of Dense version ------------------------------
-
 // -------------------------- Sparse serial version - TODO: separate to class ---
-//        Epetra_SerialComm Comm();
-//        // ***** Map puts same number of equations on each pe *****
-//        int NumMyElements = 1000 ;
-//        Epetra_Map Map(-1, NumMyElements, 0, Comm);
-//        int NumGlobalElements = Map.NumGlobalElements();
+        Epetra_SerialComm comm;
+        // map puts same number of equations on each pe
+        Epetra_Map epeMap(-1, numOfRows, 0, comm);
+        int numGlobalElements = epeMap.NumGlobalElements();
+        int numMyElements = epeMap.NumMyElements();
 
-//        // ***** Create an Epetra_Matrix  tridiag(-1,2,-1) *****
+        // ***** Create an Epetra_Matrix tridiag(-1,2,-1) *****
+        Epetra_CrsMatrix epeA(Copy, epeMap, 3);
+        std::cout << epeA << std::endl;
 
-//        Epetra_CrsMatrix A(Copy, Map, 3);
-//        double negOne = -1.0; double posTwo = 2.0;
+        int globalRow;
 
-//          for (int i=0; i<NumMyElements; i++) {
-//            int GlobalRow = A.GRID(i);
-//            int RowLess1 = GlobalRow - 1;
-//            int RowPlus1 = GlobalRow + 1;
-//            if (RowLess1!=-1)
-//               A.InsertGlobalValues(GlobalRow, 1, &negOne, &RowLess1);
-//            if (RowPlus1!=NumGlobalElements)
-//               A.InsertGlobalValues(GlobalRow, 1, &negOne, &RowPlus1);
-//            A.InsertGlobalValues(GlobalRow, 1, &posTwo, &GlobalRow);
-//          }
-//        A.FillComplete(); // Transform from GIDs to LIDs
+        for (int i = 0; i < numOfRows; i++) {
+            globalRow = epeA.GRID(i);
+            // epeA.InsertGlobalValues(GlobalRow, 1, &posTwo, &GlobalRow);
+        }
+        epeA.FillComplete(); // Transform from GIDs to LIDs
 //        // ***** Create x and b vectors *****
-//          Epetra_Vector x(Map);
-//          Epetra_Vector b(Map);
-//          b.Random(); // Fill RHS with random #s
-//          // ***** Create Linear Problem *****
-//            Epetra_LinearProblem problem(&A, &x, &b);
-//            // ***** Create/define AztecOO instance, solve *****
-//            AztecOO solver(problem);
-//            solver.SetAztecOption(AZ_precond, AZ_Jacobi);
-//            solver.Iterate(1000, 1.0E-8);
-//            // ***** Report results, finish ***********************
-//              cout << "Solver performed " << solver.NumIters()
-//                      << " iterations." << endl
-//                      << "Norm of true residual = "
-//                      << solver.TrueResidual()
-//                      << endl;
-// -------------------------- END of Sparse Serial version ------------------------------
+//        Epetra_Vector epeX(epeMap);
+//        Epetra_Vector epeB(epeMap);
+//        b.Random(); // Fill RHS with random #s
+//        // ***** Create Linear Problem *****
+//        Epetra_LinearProblem problem(&epeA, epeX, &epeB);
 
-// -------------------------- Distributed version - TODO: separate to class -----
+//        // ------ Amesos solver
+//        Amesos_BaseSolver* solver;
 
-//        Epetra_Map mapOnProcess()
+//        Amesos factory;
+//        char* solverType = "Amesos_Klu"; // uses the KLU direct solver
+//        solver = factory.Create(solverType, problem);
 
+//        AMESOS_CHK_ERR(solver->SymbolicFactorization());
 
-//        Amesos_BaseSolver* Solver;
-//        Amesos Factory;
-//        char* SolverType = "Amesos_Klu"; // uses the KLU direct solver
-        // Solver = Factory.Create(SolverType, Problem);
+//        AMESOS_CHK_ERR(solver->NumericFactorization());
+//        AMESOS_CHK_ERR(solver->Solve());
+//        std::cout << x << std::endl;
 
-//        AMESOS_CHK_ERR(Solver->SymbolicFactorization());
+        // ------ End of Amesos
+        // ------ AztecOO solver
+        //        AztecOO solver(problem);
+        //        solver.SetAztecOption(AZ_precond, AZ_Jacobi);
+        //        solver.Iterate(1000, 1.0E-8);
+        //        // ***** Report results, finish ***********************
+        //        std::cout << "Solver performed " << solver.NumIters() << " iterations." << std::endl << "Norm of true residual = " << solver.TrueResidual() << std::endl;
+        // ------ End of AztecOO
+        // -------------------------- END of Sparse Serial version ------------------------------
 
-//        AMESOS_CHK_ERR(Solver->NumericFactorization());
-//        AMESOS_CHK_ERR(Solver->Solve());
+        // -------------------------- Distributed version - TODO: separate to class -----
+        //        Amesos_BaseSolver* Solver;
+        //        Amesos Factory;
+        //        char* SolverType = "Amesos_Klu"; // uses the KLU direct solver
+        //        Solver = Factory.Create(SolverType, Problem);
 
-//        std::ofstream writeSln(solutionArg.getValue());
-//        solution.block_write(writeSln);
-//        writeSln.close();
+        //        AMESOS_CHK_ERR(Solver->SymbolicFactorization());
 
-// -------------------------- END of Sparse Distributed version ------------------------------
+        //        AMESOS_CHK_ERR(Solver->NumericFactorization());
+        //        AMESOS_CHK_ERR(Solver->Solve());
+
+        // -------------------------- END of Sparse Distributed version ------------------------------
+        std::ofstream writeSln(solutionArg.getValue());
+        solution.block_write(writeSln);
+        writeSln.close();
+
         exit(0);
     }
     catch (TCLAP::ArgException &e)
