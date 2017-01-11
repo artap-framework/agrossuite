3baa6f3df6ee1692ac9897b6885c2eac48fbcc4b
Pavel Karban <pkarban@gmail.com>
2017-01-11 15:14:41 +0100

    cdash removed

--- resources/python/CTestConfig.cmake
+++ resources/python/CTestConfig.cmake
@@ -9,7 +9,7 @@
 set(CTEST_PROJECT_NAME "Agros2D")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
-set(CTEST_DROP_METHOD "https")
-set(CTEST_DROP_SITE "edison.fel.zcu.cz:999")
-set(CTEST_DROP_LOCATION "/submit.php?project=Agros2D")
-set(CTEST_DROP_SITE_CDASH TRUE)
+# set(CTEST_DROP_METHOD "https")
+# set(CTEST_DROP_SITE "edison.fel.zcu.cz:999")
+# set(CTEST_DROP_LOCATION "/submit.php?project=Agros2D")
+# set(CTEST_DROP_SITE_CDASH TRUE)
