efe6d5477413c634f40ab33209f3a9d7132eebb2
Pavel Karban <pkarban@gmail.com>
2017-03-05 22:04:40 +0100

    fixed path in paralution

--- solver-external-paralution/CTestConfig.cmake
+++
@@ -1,6 +0,0 @@
-set(CTEST_PROJECT_NAME "Agros2D - PARALUTION")
-
-set(CTEST_DROP_METHOD "https")
-set(CTEST_DROP_SITE "edison.fel.zcu.cz:999")
-set(CTEST_DROP_LOCATION "/submit.php?project=Agros2D")
-set(CTEST_DROP_SITE_CDASH TRUE)
