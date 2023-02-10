00dc49ca4b85493816016180c086c1d8dd1e66e8
Pavel Karban <karban@kte.zcu.cz>
2023-02-10 13:55:30 +0100

    stacktrace removed

--- agros-library/util/global.cpp
+++ agros-library/util/global.cpp
@@ -38,14 +38,6 @@
 #define __USE_GNU
 #endif
 
-#include <execinfo.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ucontext.h>
-#include <unistd.h>
-
 #ifdef Q_WS_WIN
 #include "Windows.h"
 #pragma comment(lib, "psapi.lib")
@@ -116,75 +108,9 @@ QStringList solverList(const QString &data)
     return list;
 }
 
-/* This structure mirrors the one found in /usr/include/asm/ucontext.h */
-typedef struct _sig_ucontext {
-    unsigned long     uc_flags;
-    struct ucontext   *uc_link;
-    stack_t           uc_stack;
-    struct sigcontext uc_mcontext;
-    sigset_t          uc_sigmask;
-} sig_ucontext_t;
-
-void crit_err_hdlr(int sig_num, siginfo_t * info, void * ucontext) {
-    void *             array[50];
-    void *             caller_address;
-    char **            messages;
-    int                size, i;
-    sig_ucontext_t *   uc;
-
-    uc = (sig_ucontext_t *)ucontext;
-
-    /* Get the address at the time the signal was raised */
-#if defined(__i386__) // gcc specific
-    caller_address = (void *) uc->uc_mcontext.eip; // EIP: x86 specific
-#elif defined(__x86_64__) // gcc specific
-    caller_address = (void *) uc->uc_mcontext.rip; // RIP: x86_64 specific
-#else
-#error Unsupported architecture. // TODO: Add support for other arch.
-#endif
-
-    fprintf(stderr, "\n");
-    FILE * backtraceFile;
-
-    // In this example we write the stacktrace to a file. However, we can also just fprintf to stderr (or do both).
-    QString backtraceFilePath = "/tmp/stacktrace.txt;";
-    backtraceFile = fopen(backtraceFilePath.toUtf8().data(),"w");
-
-    if (sig_num == SIGSEGV)
-        fprintf(backtraceFile, "signal %d (%s), address is %p from %p\n",sig_num, strsignal(sig_num), info->si_addr,(void *)caller_address);
-    else
-        fprintf(backtraceFile, "signal %d (%s)\n",sig_num, strsignal(sig_num));
-
-    size = backtrace(array, 50);
-    /* overwrite sigaction with caller's address */
-    array[1] = caller_address;
-    messages = backtrace_symbols(array, size);
-    /* skip first stack frame (points here) */
-    for (i = 1; i < size && messages != NULL; ++i) {
-        fprintf(backtraceFile, "[bt]: (%d) %s\n", i, messages[i]);
-    }
-
-    fclose(backtraceFile);
-    free(messages);
-
-    exit(EXIT_FAILURE);
-}
-
-void installSignal(int __sig) {
-    struct sigaction sigact;
-    sigact.sa_sigaction = crit_err_hdlr;
-    sigact.sa_flags = SA_RESTART | SA_SIGINFO;
-    if (sigaction(__sig, &sigact, (struct sigaction *)NULL) != 0) {
-        fprintf(stderr, "error setting signal handler for %d (%s)\n",__sig, strsignal(__sig));
-        exit(EXIT_FAILURE);
-    }
-}
-
 void initSingleton()
 {
     // For crashes, SIGSEV should be enough.
-    installSignal(SIGSEGV);
-
     setlocale(LC_NUMERIC, "C");
 
     // char *argv[] = {(char *) QString("%1/agros_python").arg(getenv("PWD")).toStdString().c_str(), NULL};
